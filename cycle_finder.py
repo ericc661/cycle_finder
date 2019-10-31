ef4e5a2ae92683604bb3a9aa84daa4e3cd83f0ba
Eric Chen <ericc661@gmail.com>
2019-10-30 23:43:11 -0400

    getting started, add testfiles. need to add unit test file

---
+++ cycle_finder.py
@@ -0,0 +1,15 @@
+
+class CycleFinder(object):
+
+    # TODO: read in matrix from input and store as member variable
+    # test this
+    # TODO: handle errors - invalid file, bad matrix, etc.
+    def __init__(self, file):
+        print('hello')
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
